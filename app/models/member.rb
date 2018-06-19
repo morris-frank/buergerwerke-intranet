7f67662a6bf56419f55287eb981df1ae1e9d43f7
Maurice Frank <maurice.frank@posteo.de>
2018-06-19 16:44:15 +0200

    Added devise_invitable

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,8 @@
 class Member < User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable :validatable
-  # devise :invitable, :confirmable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :invitable, :confirmable, :registerable
+  # devise :invitable, :confirmable, :registerable
   belongs_to :cooperative
   has_and_belongs_to_many :groups, joint_table: :groups_users
 end
