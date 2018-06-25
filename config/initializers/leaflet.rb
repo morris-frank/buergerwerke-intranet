893cfd58841a52de416ac16468fc66075f9355d1
Maurice Frank <maurice.frank@posteo.de>
2018-06-25 12:58:36 +0200

    Added more providees

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,5 +1,15 @@
-# Leaflet.tile_layer = "https://maps.wikimedia.org/osm-intl/${z}/${x}/${y}.png"
-Leaflet.tile_layer = "https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png"
-Leaflet.attribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-Leaflet.max_zoom = 18
-Leaflet.subdomains = ['a','b','c','d']
+# Stamen Terrain:
+# Leaflet.tile_layer = "https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png"
+# Leaflet.attribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+# Leaflet.max_zoom = 18
+# Leaflet.subdomains = ['a','b','c','d']
+
+# OSM
+# Leaflet.tile_layer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+# Leaflet.attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+# Leaflet.max_zoom = 19
+
+# OpenMapSuerfer HD
+Leaflet.tile_layer = 'https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}'
+Leaflet.attribution = 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+Leaflet.max_zoom = 20
