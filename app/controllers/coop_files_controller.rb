8358394bd3ed89316931b89f338099c5f90a741e
Maurice Frank <maurice.frank@posteo.de>
2018-07-05 14:56:21 +0200

    ADD: missing edit for file categories

--- app/controllers/coop_files_controller.rb
+++ app/controllers/coop_files_controller.rb
@@ -3,7 +3,7 @@ class CoopFilesController < ApplicationController
 
     def index
         @cooperative = Cooperative.find(params[:cooperative_id])
-        @files = @cooperative.fileclips
+        @files_grouped = @cooperative.fileclips.group_by(&:file_category)
 
         if @cooperative.id != current_member.cooperative_id
             redirect_to cooperative_path(@cooperative)
