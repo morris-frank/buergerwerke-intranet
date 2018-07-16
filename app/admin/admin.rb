0a66a54bca66c018bdc5d45039323b74a448cc1f
Maurice Frank <maurice.frank@posteo.de>
2018-07-16 15:12:14 +0200

    Updated admin aa view

--- app/admin/admin.rb
+++ app/admin/admin.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Admin do
-    permit_params :email, :password, :password_confirmation
+    permit_params :email, :password, :password_confirmation, :firstname, :lastname
 
     index do
       selectable_column
@@ -10,12 +10,24 @@ ActiveAdmin.register Admin do
       actions
     end
 
+    show do
+      attributes_table do
+        row :firstname
+        row :lastname
+        row :email
+        row :created_at
+        row :updated_at
+      end
+    end
+
     filter :firstname
     filter :lastname
     filter :email
 
     form do |f|
       f.inputs "Admin Details" do
+        f.input :firstname
+        f.input :lastname
         f.input :email
         f.input :password
         f.input :password_confirmation
