7ca4cadf8a17f3bfdf713341011e754bbd944384
Maurice Frank <maurice.frank@posteo.de>
2018-07-25 10:12:19 +0200

    Removed Empty Test files

--- test/controllers/cooperatives_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CooperativesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/plants_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PlantsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
