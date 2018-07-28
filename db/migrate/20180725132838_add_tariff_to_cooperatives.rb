b83e6e64059384b2f6fa044d0a1b99328df048d8
Maurice Frank <maurice.frank@posteo.de>
2018-07-28 19:13:39 +0200

    Boolean column removing null val

--- db/migrate/20180725132838_add_tariff_to_cooperatives.rb
+++ db/migrate/20180725132838_add_tariff_to_cooperatives.rb
@@ -1,5 +1,5 @@
 class AddTariffToCooperatives < ActiveRecord::Migration[5.2]
   def change
-    add_column :cooperatives, :has_tariff, :boolean
+    add_column :cooperatives, :has_tariff, :boolean, null: false, default: false
   end
 end
