de742592babe0485545b59759c278552e9214680
Maurice Frank <maurice.frank@posteo.de>
2018-07-30 00:41:48 +0200

    Added local ENV File with smtp settings

--- config/application.rb
+++ config/application.rb
@@ -2,21 +2,20 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module BuergerwerkeIntranet
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     config.load_defaults 5.2
 
     config.site_title = 'Intranet der Bürgerwerke'
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
     config.i18n.default_locale = :'de'
   end
 end
