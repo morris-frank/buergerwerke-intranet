38fcce99204d22840fe9b3fce3a1cbbfbc97d0b1
Maurice Frank <maurice.frank@posteo.de>
2018-07-31 15:12:14 +0200

    Frontpage Nicer, fixes #56

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,5 @@ class HomeController < ApplicationController
     before_action :authenticate_member!
 
     def index
-      @newest_coop_files = current_member.cooperative.fileclips.last(5)
     end
   end
