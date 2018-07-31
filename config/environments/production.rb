5fee462f2aae47948942cb22b9571b386de56c96
Maurice Frank <maurice.frank@posteo.de>
2018-07-31 16:44:13 +0200

    added default host config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { :host => ENV['DEFAULT_URL_HOST'] }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
