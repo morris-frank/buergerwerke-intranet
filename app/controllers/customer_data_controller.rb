fc07944710dc6a92fb6810d64839bf70d611e348
Maurice Frank <maurice.frank@posteo.de>
2018-08-01 16:43:15 +0200

    show all member customer data attributes

--- app/controllers/customer_data_controller.rb
+++ app/controllers/customer_data_controller.rb
@@ -11,5 +11,7 @@ class CustomerDataController < ApplicationController
         if ! current_member.can_see_customer_data
             redirect_to cooperative_path(@cooperative)
         end
+
+        @cd_attributes = [:ra_company, :ra_company_co, :ra_titel, :ra_firstname, :ra_lastname, :ra_street, :ra_zip, :ra_city, :ra_mail, :ls_firstname, :ls_lastname, :ls_street, :ls_zip, :ls_city, :meter_number, :consumption_ht, :consumption_nt, :meter_process, :meter_read_date, :t_id, :accepted_privacy_statement, :status, :deliver_from, :free_from, :provider, :customer_origin, :customer_number, :input_date, :running_number]
     end
 end
