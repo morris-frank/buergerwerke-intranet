4e225cd009a0f47082c562bd55df9d62e72a94c4
Maurice Frank <maurice.frank@posteo.de>
2018-08-30 10:21:33 +0200

    moved js fn out of domready

--- app/assets/javascripts/bulma.js
+++ app/assets/javascripts/bulma.js
@@ -1,47 +1,43 @@
-domready(function () {
-  // Burger Menu
-  var burgers = document.getAll('.navbar-burger');
+function setBurgerListeners(burgers) {
+  burgers.forEach(function (burger) {
+    burger.addEventListener('click', function () {
+      this.classList.toggle('is-active');
+      var target = document.getElementById(this.dataset.target);
+      target.classList.toggle('is-active');
+    });
+  });
+}
 
-  if (burgers.length > 0) setBurgerListeners();
+function setModalButtonListeners(modalButtons) {
+  modalButtons.forEach(function (modalButton) {
+    modalButton.addEventListener('click', function () {
+      var modal = document.getElementById(this.dataset.target);
+      document.documentElement.classList.add('is-clipped');
+      modal.classList.add('is-active');
+    });
+  });
+}
 
-  function setBurgerListeners() {
-    burgers.forEach(function (burger) {
-      burger.addEventListener('click', function () {
-        this.classList.toggle('is-active');
-        var target = document.getElementById(this.dataset.target);
-        target.classList.toggle('is-active');
+function setModalCloserListeners(modalClosers) {
+  var modals = document.getAll('.modal');
+  modalClosers.forEach(function (modalCloser) {
+    modalCloser.addEventListener('click', function () {
+      document.documentElement.classList.remove('is-clipped');
+      modals.forEach(function (modal) {
+        modal.classList.remove('is-active');
       });
     });
-  }
+  });
+}
 
-  // Modals
-  var modals = document.getAll('.modal');
+domready(function () {
+  var burgers = document.getAll('.navbar-burger');
+  if (burgers.length > 0) setBurgerListeners(burgers);
+
   var modalButtons = document.getAll('.modal-button');
   var modalClosers = document.getAll('.modal-background, .modal-close');
-
   if (modalButtons.length > 0 && modalClosers.length > 0) {
-    setModalButtonListeners();
-    setModalCloserListeners();
-  }
-
-  function setModalButtonListeners() {
-    modalButtons.forEach(function (modalButton) {
-      modalButton.addEventListener('click', function () {
-        var modal = document.getElementById(this.dataset.target);
-        document.documentElement.classList.add('is-clipped');
-        modal.classList.add('is-active');
-      });
-    });
-  }
-
-  function setModalCloserListeners() {
-    modalClosers.forEach(function (modalCloser) {
-      modalCloser.addEventListener('click', function () {
-        document.documentElement.classList.remove('is-clipped');
-        modals.forEach(function (modal) {
-          modal.classList.remove('is-active');
-        });
-      });
-    });
+    setModalButtonListeners(modalButtons);
+    setModalCloserListeners(modalClosers);
   }
 });
--- app/assets/javascripts/table-filter.js
+++ app/assets/javascripts/table-filter.js
@@ -1,55 +1,50 @@
-domready(function () {
+function setFilterListeners(filters) {
+	filters.forEach(function (filter) {
+		filter.addEventListener('input', function () {
+			var searchString = filter.value.trim().toLowerCase();
+			filter_tables(searchString);
+		});
+	});
+}
+
+function filter_tables(searchString) {
 	var tables = document.getAll('table.is-filterable');
-	var filters = document.getAll('.table-filter');
-
-	if (filters.length > 0) setFilterListeners();
-
-	function setFilterListeners() {
-		filters.forEach(function (filter) {
-			filter.addEventListener('input', function () {
-				var searchString = filter.value.trim().toLowerCase();
-				filter_tables(searchString);
-			});
-		});
-	}
-
-	function filter_tables(searchString) {
-		tables.forEach(function (table) {
-			filter_table(table, searchString);
-		});
-	}
-
-	function filter_table(table, searchString) {
+	tables.forEach(function (table) {
 		for (var i = 0, row; row = table.rows[i]; i++) {
 			var text = row.textContent.trim().toLowerCase();
 			row.style.display = text.indexOf(searchString) === -1 ? 'none' : 'table-row';
 		}
+	});
+}
+
+function setSelectsListeners(tags) {
+	tags.forEach(function (tag) {
+		tag.addEventListener('click', function () {
+			tag.classList.toggle('is-primary');
+			filter_by_tag(tag);
+		});
+		if (BROWSER_IS_WEBKIT) filter_by_tag(tag);
+	});
+}
+
+function filter_by_tag(tag) {
+	var target = '.' + tag.dataset.target;
+	var active = tag.classList.contains('is-primary');
+	document.getAll('col' + target).forEach(function (col) {
+		col.style.visibility = active ? "" : "collapse";
+	});
+	if (BROWSER_IS_WEBKIT) {
+		document.getAll(target).forEach(function (td) {
+			td.style.display = active ? "" : "none";
+		});
 	}
+}
+
+domready(function () {
+	var filters = document.getAll('.table-filter');
+	if (filters.length > 0) setFilterListeners(filters);
+
 
 	var tags = document.getAll('.table-select .tag');
-
-	if (tags.length > 0) setSelectsListeners();
-
-	function setSelectsListeners() {
-		tags.forEach(function (tag) {
-			tag.addEventListener('click', function () {
-				tag.classList.toggle('is-primary');
-				filter_by_tag(tag);
-			});
-			if (BROWSER_IS_WEBKIT) filter_by_tag(tag);
-		});
-	}
-
-	function filter_by_tag(tag) {
-		var target = '.' + tag.dataset.target;
-		var active = tag.classList.contains('is-primary');
-		document.getAll('col' + target).forEach(function (col) {
-			col.style.visibility = active ? "" : "collapse";
-		});
-		if (BROWSER_IS_WEBKIT) {
-			document.getAll(target).forEach(function (td) {
-				td.style.display = active ? "" : "none";
-			});
-		}
-	}
+	if (tags.length > 0) setSelectsListeners(tags);
 });
