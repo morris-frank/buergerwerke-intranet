dad8d4f7192e99c6c29078e9b23be0ed8d5c43db
Maurice Frank <maurice.frank@posteo.de>
2019-02-06 18:30:01 +0100

    added new scroller js and fixed table layout switsch, fixes #101, #100, #103

--- app/assets/javascripts/customer-data.js
+++ app/assets/javascripts/customer-data.js
@@ -16,19 +16,42 @@ document.addEventListener("turbolinks:load", function() {
 
 	var outerContainer = document.getElementById('customer-data-table');
 	var container = outerContainer.querySelector('.table-container');
-
-	var scroller = outerContainer.querySelector('.scroller');
-	scroller.addEventListener('scroll', function() {
-		var scrolled = scroller.scrollLeft / scroller.scrollWidth;
-		container.scrollLeft = scrolled * container.scrollWidth;
-	});
-
 	var table = outerContainer.querySelector('.table');
 	var selector = outerContainer.querySelector('.selectors');
+	var scroller = outerContainer.querySelector('.scroller');
+	var clickedSelectors = 4;
+
+	function SyncScrollerScroll() {
+		var scrolled = scroller.scrollLeft / scroller.scrollWidth;
+		container.scrollLeft = scrolled * container.scrollWidth;
+	}
+
+	function SyncContainerScroll(){
+		var scrolled = container.scrollLeft / container.scrollWidth;
+		scroller.scrollLeft = scrolled * scroller.scrollWidth;
+	}
+
+	scroller.children[0].style.width = table.scrollWidth + 'px';
+	container.addEventListener('scroll', SyncContainerScroll);
+	scroller.addEventListener('mouseenter', function () {
+		scroller.addEventListener('scroll', SyncScrollerScroll);
+		container.removeEventListener('scroll', SyncContainerScroll);
+	});
+	scroller.addEventListener('mouseleave', function () {
+		scroller.removeEventListener('scroll', SyncScrollerScroll);
+		container.addEventListener('scroll', SyncContainerScroll);
+	});
+
 	selector.querySelectorAll('.tag').forEach(function (tag) {
 		tag.addEventListener('click', function () {
 			filter_by_tag(tag, table, scroller);
+			if (BROWSER_IS_WEBKIT) filter_by_tag(tag, table, scroller);
+			clickedSelectors += -1 + 2 * tag.classList.contains('is-primary');
+			if (clickedSelectors > 6) {
+				table.classList.remove('fixed');
+			}else{
+				table.classList.add('fixed');
+			}
 		});
-		if (BROWSER_IS_WEBKIT) filter_by_tag(tag, table, scroller);
 	});
 });
